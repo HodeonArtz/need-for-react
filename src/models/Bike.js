9dc85106ac2a47741c0117c94d020151820f53cc
HodeonArtz <martidicianolold@gmail.com>
2025-01-13 19:09:50 +0100

    feat(models): create model for Bike

---
+++ src/models/Bike.js
@@ -0,0 +1,47 @@
+import { tryProbability } from "../utils/index.js";
+import Racetrack from "./Racetrack.js";
+import Vehicle from "./Vehicle";
+
+export default class Bike extends Vehicle {
+  constructor(model, traction, minMoves, maxMoves) {
+    super(model, traction, minMoves, maxMoves);
+    this.isFallen = false;
+  }
+
+  /**
+   * @param {import("./Racetrack.js").default} circuit
+   * @returns boolean
+   */
+  tryBikeFall(circuit) {
+    let bikeFallPercentage = 0.05;
+    const percetagesCasesMap = [
+      {
+        weather: Racetrack.WEATHER.rainy,
+        traction: Vehicle.TRACTION.hard,
+        percentage: 0.3,
+      },
+      {
+        weather: Racetrack.WEATHER.wet,
+        traction: Vehicle.TRACTION.hard,
+        percentage: 0.2,
+      },
+      {
+        weather: Racetrack.WEATHER.wet,
+        traction: Vehicle.TRACTION.medium,
+        percentage: 0.1,
+      },
+    ];
+
+    percetagesCasesMap.forEach((percetageCase) => {
+      if (
+        percetageCase.weather === circuit.weather &&
+        percetageCase.traction === this.traction
+      )
+        bikeFallPercentage = percetageCase.percentage;
+    });
+
+    this.isFallen = true;
+
+    return tryProbability(bikeFallPercentage);
+  }
+}
