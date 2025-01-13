44c393d22e5ddde991e385a814fbacd533d4b656
HodeonArtz <martidicianolold@gmail.com>
2025-01-13 19:00:25 +0100

    feat(models): create model for Vehicle

---
+++ src/models/Vehicle.js
@@ -0,0 +1,20 @@
+export default class Vehicle {
+  static TRACTION = {
+    soft: "soft",
+    medium: "medium",
+    hard: "hard",
+  };
+  /**
+   *
+   * @param {string} model
+   * @param {"soft" | "medium" | "hard"} traction
+   * @param {number} minMoves
+   * @param {number} maxMoves
+   */
+  constructor(model, traction, minMoves, maxMoves) {
+    this.model = model;
+    this.traction = traction;
+    this.minMoves = minMoves;
+    this.maxMoves = maxMoves;
+  }
+}
