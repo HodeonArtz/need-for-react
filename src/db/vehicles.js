67f888f402fd90631c6d85b0896dfd614b33f928
HodeonArtz <martidicianolold@gmail.com>
2025-01-23 19:22:48 +0100

    refactor: move Vehicles to separate js file

---
+++ src/db/vehicles.js
@@ -0,0 +1,75 @@
+import { Bike, Car } from "../models";
+
+// List of default vehicles
+export const defaultVehicles = [
+  new Bike({
+    color: "#f1c40f",
+    minMoves: 3,
+    maxMoves: 6,
+    model: "Ducati Scrambler",
+    traction: "medium",
+  }),
+  new Car({
+    color: "#9b59b6",
+    minMoves: 6,
+    maxMoves: 10,
+    model: "BMW Serie 3",
+    traction: "high",
+  }),
+  new Bike({
+    color: "#3498db",
+    minMoves: 4,
+    maxMoves: 8,
+    model: "Kawasaki Ninja",
+    traction: "high",
+  }),
+  new Car({
+    color: "#3498db",
+    minMoves: 4,
+    maxMoves: 7,
+    model: "Ford Fiesta",
+    traction: "medium",
+  }),
+  new Bike({
+    color: "#2ecc71",
+    minMoves: 5,
+    maxMoves: 9,
+    model: "Yamaha MT-07",
+    traction: "medium",
+  }),
+  new Car({
+    color: "#ff6863",
+    minMoves: 3,
+    maxMoves: 6,
+    model: "Opel Corsa",
+    traction: "medium",
+  }),
+  new Bike({
+    color: "#9b59b6",
+    minMoves: 6,
+    maxMoves: 10,
+    model: "BMW R 1250 GS",
+    traction: "high",
+  }),
+  new Car({
+    color: "#f1c40f",
+    minMoves: 2,
+    maxMoves: 5,
+    model: "Volkswagen Golf",
+    traction: "medium",
+  }),
+  new Bike({
+    color: "#ff6863",
+    minMoves: 3,
+    maxMoves: 7,
+    model: "Harley Davidson Sportster",
+    traction: "medium",
+  }),
+  new Car({
+    color: "#2ecc71",
+    minMoves: 5,
+    maxMoves: 8,
+    model: "Toyota Corolla",
+    traction: "high",
+  }),
+];
--- src/hooks/useEntitiesState.js
+++ src/hooks/useEntitiesState.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { Bike, Car, Driver, Vehicle } from "../models";
+import { defaultVehicles } from "../db/vehicles";
 
 /**
  * @typedef {import("../models/Participant/Driver").default} Driver
@@ -17,79 +18,7 @@ export const useEntitiesState = create(
    */
   (set) => ({
     drivers: [new Driver({ name: "Juan" })],
-    vehicles: [
-      // Lista de coches y motos con valores coherentes
-      new Bike({
-        color: "#f1c40f",
-        minMoves: 3,
-        maxMoves: 6,
-        model: "Ducati Scrambler",
-        traction: "medium",
-      }),
-      new Car({
-        color: "#9b59b6",
-        minMoves: 6,
-        maxMoves: 10,
-        model: "BMW Serie 3",
-        traction: "high",
-      }),
-      new Bike({
-        color: "#3498db",
-        minMoves: 4,
-        maxMoves: 8,
-        model: "Kawasaki Ninja",
-        traction: "high",
-      }),
-      new Car({
-        color: "#3498db",
-        minMoves: 4,
-        maxMoves: 7,
-        model: "Ford Fiesta",
-        traction: "medium",
-      }),
-      new Bike({
-        color: "#2ecc71",
-        minMoves: 5,
-        maxMoves: 9,
-        model: "Yamaha MT-07",
-        traction: "medium",
-      }),
-      new Car({
-        color: "#ff6863",
-        minMoves: 3,
-        maxMoves: 6,
-        model: "Opel Corsa",
-        traction: "medium",
-      }),
-      new Bike({
-        color: "#9b59b6",
-        minMoves: 6,
-        maxMoves: 10,
-        model: "BMW R 1250 GS",
-        traction: "high",
-      }),
-      new Car({
-        color: "#f1c40f",
-        minMoves: 2,
-        maxMoves: 5,
-        model: "Volkswagen Golf",
-        traction: "medium",
-      }),
-      new Bike({
-        color: "#ff6863",
-        minMoves: 3,
-        maxMoves: 7,
-        model: "Harley Davidson Sportster",
-        traction: "medium",
-      }),
-      new Car({
-        color: "#2ecc71",
-        minMoves: 5,
-        maxMoves: 8,
-        model: "Toyota Corolla",
-        traction: "high",
-      }),
-    ],
+    vehicles: [...defaultVehicles],
     racetracks: [],
     addDriver: (driver) =>
       set((entities) => ({ drivers: [...entities.drivers, driver] })),
