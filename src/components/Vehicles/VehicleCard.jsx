33d72e35b98e9fd089b51d014df54c16d82dfd18
HodeonArtz <martidicianolold@gmail.com>
2025-01-23 19:20:14 +0100

    refactor: move vehicle preview and vehicle data table to components

--- src/components/Vehicles/VehicleCard.jsx
+++ src/components/Vehicles/VehicleCard.jsx
@@ -21,56 +21,65 @@ const VehicleCard = ({ vehicle }) => {
   return (
     <Card>
       <Card.Section>
-        <BackgroundImage
-          src={backgroundVehicle}
-          style={{ backgroundPositionY: "96%" }}
-          bgsz={500}
-          p="lg"
-        >
-          <Flex w="100%" justify={"center"} align={"end"}>
-            {vehicle.constructor.name === Car.name && (
-              <CarRender color={vehicle.color} width={"100%"} />
-            )}
-            {vehicle.constructor.name === Bike.name && (
-              <BikeRender color={vehicle.color} height={"100px"} />
-            )}
-          </Flex>
-        </BackgroundImage>
+        <VehiclePreview vehicle={vehicle} />
       </Card.Section>
       <Group justify="space-between" mt="md" mb="xs">
         <Title ff={"text"} size="h2">
           {vehicle.model}
         </Title>
-
-        <Table withColumnBorders variant="vertical" layout="fixed">
-          <Table.Tbody>
-            <Table.Tr>
-              <Table.Th w={100}>Color</Table.Th>
-              <Table.Td>
-                <Badge color={vehicle.color}>
-                  {closest(vehicle.color).name}
-                </Badge>
-              </Table.Td>
-            </Table.Tr>
-
-            <Table.Tr>
-              <Table.Th>Traction</Table.Th>
-              <Table.Td>{firstLetterUppercase(vehicle.traction)}</Table.Td>
-            </Table.Tr>
-
-            <Table.Tr>
-              <Table.Th>Min. Speed</Table.Th>
-              <Table.Td>{vehicle.maxMoves} moves</Table.Td>
-            </Table.Tr>
-            <Table.Tr>
-              <Table.Th>Max Speed</Table.Th>
-              <Table.Td>{vehicle.minMoves} moves</Table.Td>
-            </Table.Tr>
-          </Table.Tbody>
-        </Table>
+        <VehicleDataTable vehicle={vehicle} />
       </Group>
     </Card>
   );
 };
 
 export default VehicleCard;
+
+const VehiclePreview = ({ vehicle }) => {
+  return (
+    <BackgroundImage
+      src={backgroundVehicle}
+      style={{ backgroundPositionY: "96%" }}
+      bgsz={500}
+      p="lg"
+    >
+      <Flex w="100%" justify={"center"} align={"end"}>
+        {vehicle.constructor.name === Car.name && (
+          <CarRender color={vehicle.color} width={"100%"} />
+        )}
+        {vehicle.constructor.name === Bike.name && (
+          <BikeRender color={vehicle.color} height={"100px"} />
+        )}
+      </Flex>
+    </BackgroundImage>
+  );
+};
+
+export const VehicleDataTable = ({ vehicle }) => {
+  return (
+    <Table withColumnBorders variant="vertical" layout="fixed">
+      <Table.Tbody>
+        <Table.Tr>
+          <Table.Th w={100}>Color</Table.Th>
+          <Table.Td>
+            <Badge color={vehicle.color}>{closest(vehicle.color).name}</Badge>
+          </Table.Td>
+        </Table.Tr>
+
+        <Table.Tr>
+          <Table.Th>Traction</Table.Th>
+          <Table.Td>{firstLetterUppercase(vehicle.traction)}</Table.Td>
+        </Table.Tr>
+
+        <Table.Tr>
+          <Table.Th>Min. Speed</Table.Th>
+          <Table.Td>{vehicle.maxMoves} moves</Table.Td>
+        </Table.Tr>
+        <Table.Tr>
+          <Table.Th>Max Speed</Table.Th>
+          <Table.Td>{vehicle.minMoves} moves</Table.Td>
+        </Table.Tr>
+      </Table.Tbody>
+    </Table>
+  );
+};
