7f4c5253a3a32647f36d17189099570e6137339d
HodeonArtz <martidicianolold@gmail.com>
2025-01-26 01:33:00 +0100

    feat: action icon to racetrack card

---
+++ src/db/racetracks.js
@@ -0,0 +1,54 @@
+import { Racetrack } from "../models";
+
+export const defaultRacetracks = [
+  new Racetrack({
+    name: "Silverstone Circuit",
+    weather: "dry",
+    trackLengthKm: 5.89,
+  }),
+  new Racetrack({
+    name: "Monaco Grand Prix Circuit",
+    weather: "wet",
+    trackLengthKm: 3.34,
+  }),
+  new Racetrack({
+    name: "Suzuka International Racing Course",
+    weather: "rainy",
+    trackLengthKm: 5.81,
+  }),
+  new Racetrack({
+    name: "Circuit de Spa-Francorchamps",
+    weather: "rainy",
+    trackLengthKm: 7.0,
+  }),
+  new Racetrack({
+    name: "Monza Circuit",
+    weather: "dry",
+    trackLengthKm: 5.79,
+  }),
+  new Racetrack({
+    name: "Interlagos Circuit",
+    weather: "wet",
+    trackLengthKm: 4.31,
+  }),
+  new Racetrack({
+    name: "Circuit of the Americas",
+    weather: "dry",
+    trackLengthKm: 5.51,
+  }),
+  new Racetrack({
+    name: "Nürburgring Grand Prix Circuit",
+    weather: "rainy",
+    trackLengthKm: 5.15,
+  }),
+  new Racetrack({
+    name: "Red Bull Ring",
+    weather: "dry",
+    trackLengthKm: 4.32,
+  }),
+  new Racetrack({
+    name: "Albert Park Circuit",
+    weather: "wet",
+    trackLengthKm: 5.28,
+  }),
+];
--- src/pages/racetracks/RacetracksList.jsx
+++ src/pages/racetracks/RacetracksList.jsx
@@ -1,5 +1,57 @@
+import { ActionIcon, Group, Stack, Title, Tooltip } from "@mantine/core";
+import { defaultRacetracks } from "../../db/racetracks";
+import { Racetrack } from "../../models";
+import { IconCloudRain, IconDroplet, IconSun } from "@tabler/icons-react";
+import { firstLetterUppercase } from "../../utils";
+
 const RacetracksList = () => {
-  return <div>Racetracks</div>;
+  return (
+    <Stack>
+      <Title size="h2">Racetracks</Title>
+      <Stack>
+        {defaultRacetracks.map((racetrack) => (
+          <RacetrackCardLine racetrack={racetrack} key={racetrack.id} />
+        ))}
+      </Stack>
+    </Stack>
+  );
 };
 
 export default RacetracksList;
+
+const weatherMap = [
+  { weather: Racetrack.WEATHER.dry, icon: <IconSun />, color: "yellow" },
+  { weather: Racetrack.WEATHER.wet, icon: <IconDroplet />, color: "cyan" },
+  {
+    weather: Racetrack.WEATHER.rainy,
+    icon: <IconCloudRain />,
+    color: "indigo",
+  },
+];
+
+/**
+ *
+ * @param {{racetrack: import("../../models/index").Racetrack}} props
+ */
+const RacetrackCardLine = ({ racetrack }) => {
+  const weatherMapInstance = weatherMap.find(
+    (weatherCon) => weatherCon.weather === racetrack.weather
+  );
+  return (
+    <Group style={{ borderRadius: "8px" }} bg={"dark"} p={"md"}>
+      <Tooltip
+        label={firstLetterUppercase(racetrack.weather)}
+        position="left"
+        withArrow
+        color={weatherMapInstance.color}
+      >
+        <ActionIcon size={"lg"} color={weatherMapInstance.color}>
+          {weatherMapInstance.icon}
+        </ActionIcon>
+      </Tooltip>
+      <Title ff={"text"} size="h4">
+        {racetrack.name}
+      </Title>
+    </Group>
+  );
+};
